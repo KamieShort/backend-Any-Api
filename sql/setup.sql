ae4e7ad5b62dd5157fe148d1f8511892f7bac9cf
Kamie Short <kamie.short@gmail.com>
2022-06-09 09:23:44 -0700

    setting up sql and model

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+Drop table if exist beers
+
+Create table beers (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    
+)
